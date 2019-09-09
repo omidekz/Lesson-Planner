b9b154cef457c093c011c352c06c847054288898
omid <omidekz@gmail.com>
2019-09-09 23:23:11 +0430

    add select lesson in utils

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 import json
 from LessonsProgrammer import models
-
+from typing import List
 
 PACKAGES = 'packages'
 NAME = 'name'
+ListLesson = List[models.Lesson]
 
 
-def get_all_lessons():
+def get_all_lessons() -> ListLesson:
     datas = json.load(open('/home/omid/PycharmProjects/myproject/LessonsProgrammer/Lessons.json', 'r'))
     lessons = []
     for data in datas:
@@ -29,6 +30,31 @@ def get_all_lessons():
                               exam_month=exm)
         lessons.append(lesson)
     return lessons
+
+
+def select_lessons(lessons: ListLesson) -> ListLesson:
+    for ind, lesson in enumerate(lessons):
+        print('{})'.format(ind), lesson.name, '-', lesson.code)
+    new_lesson = []
+    code = input('lesson code[-1 for finish]:')
+    while code != '-1':
+        res = get(lessons, code)
+        if res and not get(new_lesson, code):
+            new_lesson.append(res)
+            print('added')
+        elif get(new_lesson, code):
+            print('it been added')
+        else:
+            print('not found this {} code'.format(code))
+        code = input('lesson code[-1 for finish]:')
+    return new_lesson
+
+
+def get(lessons: ListLesson, code):
+    for i in lessons:
+        if i.code == int(code):
+            return i
+    return None
 
 
 get_all_lessons()
