5f90124f16ace279b37ed036b81540383037b336
omid <omidekz@gmail.com>
2019-09-10 02:39:15 +0430

    make main file

---
+++ main.py
@@ -0,0 +1,10 @@
+from LessonsProgrammer import utils
+
+
+if __name__ == '__main__':
+    lessons = utils.select_lessons(utils.get_all_lessons())
+    programes = utils.make_all_possible_program(lessons)
+    for program in programes:
+        for k, v in program.days.items():
+            print(k, v)
+        print()
